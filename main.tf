7ec6d44f7aa47efd4c0bc796b426e24920ce3cab
Fernando Bohorquez <fbohorq@mapfre.com>
2022-06-28 10:49:48 +0200

    FIX: Changes

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "mapfre-gitops-fbohorq" {
 resource "aws_s3_bucket_acl" "mapfre-gitops-fbohorq" {
   bucket = aws_s3_bucket.mapfre-gitops-fbohorq.id
   acl    = "public-read"
-}
+} 
 resource "aws_s3_object" "object" {
   bucket = aws_s3_bucket.mapfre-gitops-fbohorq.id
   key    = "index.html"
